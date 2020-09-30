a921ed99dddea066adcbb5368b402b4dbc6d267d
ancyra2 <duran.talih@gmail.com>
2020-09-30 17:34:11 +0300

    Basit istatistikler eklendi

---
+++ istatistikMetotlar.R
@@ -0,0 +1,9 @@
+#Ortalamasý 10 ve standart sapmasý 1 olan rasgele normal daðýlmýþ veriler... 
+veri <- rnorm(100,mean = 10,sd = 1)
+
+#Verinin ortalamasý
+mean(veri)
+
+#Veri hakkýndan kýsa açýklayýcý istatistikler
+fivenum(veri)
+
